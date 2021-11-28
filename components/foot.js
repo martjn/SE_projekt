3b6c7dabd41aaab34cfc0657096b2308ad6df8e3
doktorjohan <johanhollak1@gmail.com>
2021-11-28 21:12:58 +0200

    make footer, more pizza

---
+++ components/foot.js
@@ -0,0 +1,33 @@
+
+
+class Footbar extends HTMLElement {
+    constructor() {
+        super();
+        this.gallery = "galerii.html"
+        this.prices = "hinnakiri.html"
+        this.landing_page = "index.html"
+        this.booking = "booking.html"
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+            <style> @import url("components/foot.css");</style>
+            <nav class="foot-bar">
+            <div class="logo">
+            <a href="${this.landing_page}">PkPR</a>
+            </div>
+            <ul class="nav-links">
+                <div class="menu">
+                    <li><a href="${this.booking}">Broneerimine</a></li>
+                    <li class="pizzas">
+                    <a href="${this.prices}">Hinnad</a>
+                    </li>
+                    <li><a href="${this.gallery}">Galerii</a></li>
+                </div>
+            </ul>
+            </nav>
+        `
+    }
+}
+
+customElements.define('foot-bar', Footbar)
