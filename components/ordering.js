1a36a42534449f1337821c4ebeb343ed86cad4a7
Heigo Tornik <heigotornik@gmail.com>
2021-11-28 18:11:03 +0200

    Finish up ordering

--- components/ordering.js
+++ components/ordering.js
@@ -1,25 +1,55 @@
-// Get the modal
-var modal = document.getElementById("ordering");
+var modalOrder = document.getElementById("ordering");
+var buttonOrder = document.getElementById("order-now-button");
+var spanOrder = document.getElementById("close-order");
+var spanSuccess = document.getElementById("close-success");
+var orderFinish = document.getElementById("order-complete")
+var modelOrderSuccess = document.getElementById("order-success")
+var ticketObj = document.getElementById("ticket")
+var timeOfArrival = document.getElementById("time-of-arrival")
 
-// Get the button that opens the modal
-var button = document.getElementById("order-now-button");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-button.onclick = function() {
-  modal.style.display = "block";
+buttonOrder.onclick = function() {
+    modalOrder.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+spanOrder.onclick = function() {
+    modalOrder.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+spanSuccess.onclick = function() {
+    modelOrderSuccess.style.display = "none";
+}
+
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+    console.log(event.target)
+  if (event.target == modalOrder) {
+    modalOrder.style.display = "none";
+  }
+  else if (event.target == modelOrderSuccess) {
+    modelOrderSuccess.style.display = "none";
   }
 } 
+
+
+
+orderFinish.onclick = function() {
+    modalOrder.style.display = "none";
+    modelOrderSuccess.style.display = "block"
+    const pizzaTimeMinutes = getRandomInt(5) * 10 + 30;
+    const arrivalTime = addMinutes(new Date(), pizzaTimeMinutes).toLocaleTimeString("et");
+    timeOfArrival.innerHTML = arrivalTime;
+    const ticketId = [0, 0, 0, 0, 0, 0].map(_ => {
+        return getRandomInt(9);
+    }).join("");
+    console.log
+    ticketObj.innerHTML = ticketId;
+
+}
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+function addMinutes(date, minutes) {
+    return new Date(date.getTime() + minutes*60000);
+}
